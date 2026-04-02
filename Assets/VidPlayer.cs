51b2275b629996bfa903d7d4d001b1e285b0070c
Zion Obcena <zmobcena@gmail.com>
2026-04-03 01:45:13 +0800

    Fixed mp4 for webgl build

---
+++ Assets/VidPlayer.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VidPlayer : MonoBehaviour
+{
+    [SerializeField] string videoFileName;
+
+    private void Start()
+    {
+        PlayVideo();
+    }
+
+    public void PlayVideo()
+    {
+        VideoPlayer videoPlayer = GetComponent<VideoPlayer>();
+
+        if (videoPlayer)
+        {
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log(videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
+    }
+}
